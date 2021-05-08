92049607e5471f2baea5ab150bebaae6b6a22375
puneetjain-iclp <puneet.jain@iclployalty.com>
2021-05-08 17:01:17 +0100

    v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h2>Hello world welcome to python flask app</h2>'
+    return '<h2>Hello world welcome to python flask app and impriving it</h2>'
 
 if __name__ == '__main__':
     app.run(threaded=True,port=5000)
